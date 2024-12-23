e4272048974a089ad5c3b30d34f304e86c5eb223
Serdar Yildirim <seyil1978@gmail.com>
2024-12-23 02:15:09 +0100

    create game logic

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,12 +2,18 @@ class Cloud extends MovableObject {
     y = 20;
     width = 500;
     height = 250;
-    
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500; //Zahl zwischen 200 und 700
-
+        this.animate();
     }
     
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1;
+        }, 1000 / 60);
+        
+    }
 }
