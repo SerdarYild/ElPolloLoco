3de96f838616e83dea1c25d0b7e9cd34fdbec46a
Serdar Yildirim <seyil1978@gmail.com>
2024-12-24 16:28:56 +0100

    move the camera

--- models/world.class.js
+++ models/world.class.js
@@ -19,6 +19,7 @@ backgroundObjects = [
 canvas;
 ctx;
 keyboard;
+camera_x = 0;
 
 constructor(canvas, keyboard) {
     this.ctx = canvas.getContext('2d');
@@ -34,10 +35,12 @@ setWorld() {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
+        this.ctx.translate(-this.camera_x, 0);
 
         //Draw() wird immer wieder aufgerufen
         let self = this;
@@ -53,6 +56,16 @@ setWorld() {
     }
 
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
